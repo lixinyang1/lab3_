38ead7990911ada9b0e1110ba7ded8c0a6791554
chenkerui <3210104320@zju.edu.cn>
2024-05-10 00:07:31 +0800

    update lab2

--- include/sa/sa.h
+++ include/sa/sa.h
@@ -15,29 +15,46 @@ class Table {
 private:
     // actually funcTable only has one map in vector
     std::vector<std::map<std::string, T>> tableVector;
+    string cur_func_name;
 public:
     Table() {
         // TODO: complete your code here
+        //do nothing yeah!
+        tableVector.clear();
+    }
+
+    void set_func_name(string x){
+        cur_func_name=x;
+    }
+
+    std::string get_cur_func_name(){
+        return cur_func_name;
     }
 
     int add_one_entry(std::string entry_name, T entry_type) {
         // TODO: complete your code here
-
+        int sz=tableVector.size();
+        tableVector[sz-1][entry_name]=entry_type;
         return 0;
     }
 
     void new_env() {
         // TODO: complete your code TODO: here
+        std::map<std::string,T>tmp;
+        tmp.clear();
+        tableVector.push_back(tmp);
     }
 
     void quit_env() {
         // TODO: complete your code here
+        tableVector.pop_back();
     }
 
     T lookup(std::string entry_name) {
         // TODO: complete your code here
-
-
+        int sz=tableVector.size();
+        if (tableVector[sz-1].count(entry_name)) return tableVector[sz-1][entry_name];
+        else return FAIL;
     }
 
 };
